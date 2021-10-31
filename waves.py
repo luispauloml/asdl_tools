36793e4315e96858e29dfa9a98519bfb32f10250
luispauloml <luispauloml@gmail.com>
2021-10-31 13:21:45 +0000

    Add wave_packet

--- waves.py
+++ waves.py
@@ -34,14 +34,51 @@ def complex_wave(disprel, freq, xs, ts):
 
 def harmonic_wave(disprel, freq, xs, ts):
     """Return the displacement of a 1D medium as a sinusoidal wave travels
-    through it
+    through it.
 
     It is the result of applyting `f(x) = -numpy.imag(x)` to the
     output of `complex_wave`, which extracts the sinus part of it and
     inverts its signal.
 
-    For more information on the arguments, see `help(complex_wave)`.
+    For more information on the other arguments, see
+    `help(complex_wave)`.
 
     """
 
     return (-np.imag(complex_wave(disprel, freq, xs, ts)))
+
+def wave_packet(disprel, freqs, xs, ts, \
+                normalize = True, \
+                amplitudes = None):
+    """Return the displacement of a 1D medium as a wave packet travels
+    through it.
+
+    It is the result of iterating `complex_wave` over `freqs`, which
+    should be a iterable containing the linear frequencies of each
+    component of the wave packet.
+
+    If `normalize` is True, the result is normalized by the maximum
+    absolute value contained in it.
+
+    If `amplitudes` is not provided, all components will have the
+    amplitude equal to one. If it is provided, it should have the same
+    length as `freqs`.
+
+    For more information on the other arguments, see
+    `help(complex_wave)`.
+
+    """
+
+    us = np.zeros((ts.size, xs.size), dtype = np.complex128)
+
+    if amplitudes is None:
+        amplitudes = np.ones((len(freqs),))
+
+    nf = len(freqs)
+    for i in range(0, nf):
+        us += amplitudes[i] * complex_wave(disprel, freqs[i], xs, ts)
+
+    if normalize:
+        return (us / np.max(np.abs(us)))
+    else:
+        return us
