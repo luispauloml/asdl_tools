d12027260f4af08eed26b65f3538d140cb334891
luispauloml <luispauloml@gmail.com>
2021-10-31 12:44:26 +0000

    Add complex_wave
    Add `complex_wave` and make `harmonic_wave` an specific case of
    `complex_wave`.

--- waves.py
+++ waves.py
@@ -3,13 +3,18 @@
 import numpy as np
 from math import pi
 
-def harmonic_wave(disprel, freq, xs, ts):
-    """Return the displacement of a medium as an harmonic wave propagates
-    through a medium.
+def complex_wave(disprel, freq, xs, ts):
+    """Return the displacement of a 1D medium as a complex harmonic wave
+    travels through it.
 
     It returns an array of shape (ts.size, xs.size) with `ts` being
     the vector for the time, and `xs` being the positions in a
     discretized 1D medium.
+
+    The complex harmonic wave is the complex exponential: 
+        exp(1j * (k*x - w*t))
+    where `k` is the angular wavenumber, `x` is the
+    position, `w` is the angular frequency and `t` is the time.
 
     `disprel` is the dispersion relationship and it should be a
     function of one argument that takes the linear frequency `freq`
@@ -19,10 +24,24 @@ def harmonic_wave(disprel, freq, xs, ts):
 
     w = 2*pi*freq               # angular frequency
     k = 2*pi*disprel(freq)      # angular wavenumer
-    us = np.zeros((ts.size, xs.size))
+    us = np.zeros((ts.size, xs.size), dtype = np.complex128)
 
     for i in range(0, ts.size):
         ps = k*xs - w*ts[i]
-        us[i, :] = (ps <= 0) * np.sin(ps)
+        us[i, :] = (ps <= 0) * np.exp(1j * ps)
 
-    return (-us)
+    return us
+
+def harmonic_wave(disprel, freq, xs, ts):
+    """Return the displacement of a 1D medium as a sinusoidal wave travels
+    through it
+
+    It is the result of applyting `f(x) = -numpy.imag(x)` to the
+    output of `complex_wave`, which extracts the sinus part of it and
+    inverts its signal.
+
+    For more information on the arguments, see `help(complex_wave)`.
+
+    """
+
+    return (-np.imag(complex_wave(disprel, freq, xs, ts)))
