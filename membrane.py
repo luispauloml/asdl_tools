698373d2c199e11bdba46faaf588111613cf0bf0
luispauloml <luispauloml@gmail.com>
2022-02-24 13:32:58 +0900

    Fix indentation

--- membrane.py
+++ membrane.py
@@ -75,113 +75,113 @@ should have a `wavepacket` object in the first position')
 should have a tuple (float, float) in the second position')
             self.__setup_source(src, pos)
 
-        def __setup_source(self, source, pos):
-            # To reduce computing time we set the domain of the source
-            # as:
-            #    - [0, d_max] if the source is inside the membrane
-            #    - [d_min, d_max] if the source is outside the
-            #      membrane domain
-            # where d_min and d_max are the minimum and the maximum
-            # distance from the source to the edges of the memebrane
-            # Then we interpolate the displacement according to the
-            # distance from a point (x, y) to the position of the source.
+    def __setup_source(self, source, pos):
+        # To reduce computing time we set the domain of the source
+        # as:
+        #    - [0, d_max] if the source is inside the membrane
+        #    - [d_min, d_max] if the source is outside the
+        #      membrane domain
+        # where d_min and d_max are the minimum and the maximum
+        # distance from the source to the edges of the memebrane
+        # Then we interpolate the displacement according to the
+        # distance from a point (x, y) to the position of the source.
 
-            # Calculate maximum distance
-            dx = self.__grid[0] - pos[0]
-            dy = self.__grid[1] - pos[1]
-            dist = np.sqrt(dx ** 2 + dy ** 2)
-            d_max = np.max(dist)
+        # Calculate maximum distance
+        dx = self.__grid[0] - pos[0]
+        dy = self.__grid[1] - pos[1]
+        dist = np.sqrt(dx ** 2 + dy ** 2)
+        d_max = np.max(dist)
 
-            # Check if `pos` is inside the domain of the membrane
-            if self.__xs[0] <= pos[0] <= self.__xs[-1] \
-               and self.__ys[0] <= pos[1] <= self.__ys[-1]:
-                d_min = 0
-            else:
-                d_min = np.min(dist)
+        # Check if `pos` is inside the domain of the membrane
+        if self.__xs[0] <= pos[0] <= self.__xs[-1] \
+           and self.__ys[0] <= pos[1] <= self.__ys[-1]:
+            d_min = 0
+        else:
+            d_min = np.min(dist)
 
-            source.dx = self.dx
-            source.fs = self.fs
-            source.set_space((d_min, d_max))
-            source.set_time(T)
-            self.__sources.append((source, pos, dist))
+        source.dx = self.dx
+        source.fs = self.fs
+        source.set_space((d_min, d_max))
+        source.set_time(self.__period)
+        self.__sources.append((source, pos, dist))
 
-        def __getPos(self, coord, shape):
-            """Return the position vector or matrix."""
+    def __getPos(self, coord, shape):
+        """Return the position vector or matrix."""
 
-            if coord not in ['x', 'y']:
-                raise ValueError("`coord` should be either 'x' or 'y'")
-            if shape not in ['vector', 'grid']:
-                raise ValueError("`shape` should be either 'vector' or \
+        if coord not in ['x', 'y']:
+            raise ValueError("`coord` should be either 'x' or 'y'")
+        if shape not in ['vector', 'grid']:
+            raise ValueError("`shape` should be either 'vector' or \
 'grid'")
-            if coord == 'x':
-                if coord == 'vector':
-                    return self.__xs
-                else:
-                    return self.__grid[0]
+        if coord == 'x':
+            if coord == 'vector':
+                return self.__xs
             else:
-                if coord == 'vector':
-                    return self.__ys
-                else:
-                    return self.__grid[1]
+                return self.__grid[0]
+        else:
+            if coord == 'vector':
+                return self.__ys
+            else:
+                return self.__grid[1]
 
-        def getX(self, shape = 'grid'):
-            """Return a 2-D matrix with the x coordinates of memebrane.
+    def getX(self, shape = 'grid'):
+        """Return a 2-D matrix with the x coordinates of memebrane.
 
-            Parameters:
-            shape : string, optional, default: 'grid'
-                If `shape` is 'vector' a vector is returned,
-                otherwise, a 2-D matrix is return.
+        Parameters:
+        shape : string, optional, default: 'grid'
+            If `shape` is 'vector' a vector is returned,
+            otherwise, a 2-D matrix is return.
 
-            """
+        """
 
-            return self.__getPos('x', shape)
+        return self.__getPos('x', shape)
 
-            return self.__grid[0]
+        return self.__grid[0]
 
-        def getY(self, fomart = 'grid'):
-            """Return a 2-D matrix with the y coordinates of memebrane.
+    def getY(self, fomart = 'grid'):
+        """Return a 2-D matrix with the y coordinates of memebrane.
 
-            Parameters:
-            shape : string, optional, default: 'grid'
-                If `shape` is 'vector' a vector is returned,
-                otherwise, a 2-D matrix is return.
+        Parameters:
+        shape : string, optional, default: 'grid'
+            If `shape` is 'vector' a vector is returned,
+            otherwise, a 2-D matrix is return.
 
-            """
+        """
 
-            return self.__getPos('y', shape)
+        return self.__getPos('y', shape)
 
-        def eval(self):
-            """Evaluate the displacement of the memebrane."""
+    def eval(self):
+        """Evaluate the displacement of the memebrane."""
 
-            data = np.zeros((self.__xs.size, \
-                             self.__ys.size, \
-                             self.__time.size))
+        data = np.zeros((self.__xs.size, \
+                         self.__ys.size, \
+                         self.__time.size))
 
-            for src, pos, dist in self.__sources:
-                src.eval()
-                src_disp = src.get_data()
-                src_domain = src.get_space()
+        for src, pos, dist in self.__sources:
+            src.eval()
+            src_disp = src.get_data()
+            src_domain = src.get_space()
 
-                for i, t in enumerate(self.__time):
-                    data[:,:,i] += np.interp(dist, src_domain, src_disp[i,:])
+            for i, t in enumerate(self.__time):
+                data[:,:,i] += np.interp(dist, src_domain, src_disp[i,:])
 
-            self.__data = data
+        self.__data = data
 
-        def get_data(self):
-            """Returns the time history of the membrane.
+    def get_data(self):
+        """Returns the time history of the membrane.
 
-            Returns a matrix of shape (nX, nY, nT) with
-                `nT = T * fs`
-                `nX = Lx // dx` and
-                `nY = Ly // dx`
-            where `T` is the total travel time, `fs` is the sampling
-            frequency, `Lx` and `Ly` ar the length of the membrane in
-            the x and y directions, respectively.  Therefore, each
-            page `k` of the matrix is the "photograph" of the
-            displacement of the domain in instante `k / fs`.  Each
-            element (i, j, k) can be mapped to a position via the
-            index (i, j) in the matrices obtained by `getX` and
-            `getY` methods.
-            """
+        Returns a matrix of shape (nX, nY, nT) with
+            `nT = T * fs`
+            `nX = Lx // dx` and
+            `nY = Ly // dx`
+        where `T` is the total travel time, `fs` is the sampling
+        frequency, `Lx` and `Ly` ar the length of the membrane in
+        the x and y directions, respectively.  Therefore, each
+        page `k` of the matrix is the "photograph" of the
+        displacement of the domain in instante `k / fs`.  Each
+        element (i, j, k) can be mapped to a position via the
+        index (i, j) in the matrices obtained by `getX` and
+        `getY` methods.
+        """
 
-            return self.__data
+        return self.__data
