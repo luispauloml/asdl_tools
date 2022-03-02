c51caca85904333ddc6a7127cbc44585308fde50
luispauloml <luispauloml@gmail.com>
2022-03-02 21:58:03 +0900

    Prepare packaging for pip

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='waves',
+    version='0.1.0',
+    description='Simple 1D and 2D waves',
+    long_description=readme,
+    author='Luis Paulo Morais Lima',
+    author_email='luispauloml@gmail.com',
+    url='https://github.com/luispauloml/waves',
+    license=license,
+    packages=find_packages()
+)
---
+++ waves/__init__.py
@@ -0,0 +1,7 @@
+"""
+Simple 1D and 2D waves.
+"""
+
+
+from waves.wavepacket import Wavepacket
+from waves.membrane import Membrane
