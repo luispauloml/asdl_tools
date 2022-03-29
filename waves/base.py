50bf693432a0ccc9025a4e2d8c5cee80c1fbe29a
luispauloml <luispauloml@gmail.com>
2022-03-29 20:13:52 +0900

    Add save and load methods to MeasuredData

--- waves/base.py
+++ waves/base.py
@@ -1,9 +1,20 @@
 import numbers
 import numpy as np
+import pickle
 from collections.abc import Iterable
 
 class MeasuredData(object):
     """Objects to stored measured or computed values"""
+
+    def load(file_name):
+        """Load data from a saved object.
+
+        NOTE: the loaded object will be an instance of MeasuredData.
+
+        """
+        with open(file_name, 'rb') as file_:
+            obj = pickle.load(file_)
+        return obj
 
     def __init__(self):
         self._data = {'space': [None, None], 'time': None, 'results': None}
@@ -82,6 +93,30 @@ class MeasuredData(object):
     def y_vect(self):
         """a vector of the discretization of space in the y direction"""
         return self._data['space'][1]
+
+    def save(self, file_name):
+        """Save data from current object to a binary file.
+
+        If the object being saved is an instance of a subclass of
+        MeasuredData, it will be an instance of MeasuredData after
+        being reloaded, and not of the original class.
+
+        NOTE: this method always overwrite existing files.
+
+        Parameters:
+        file_name : str
+            The name of the file where the data will be stored.
+
+        """
+        obj = MeasuredData()
+        for key in obj.__dict__.keys():
+            obj.__dict__[key] = self.__dict__[key]
+
+        # Guarantee that the data saved is real not complex
+        obj._data['results'] = self.data
+
+        with open(file_name, 'wb') as file_:
+            pickle.dump(obj, file_, pickle.HIGHEST_PROTOCOL)
 
 class BaseWave(MeasuredData):
     """Base class to define Wavepacket and Surface"""
