58fee7b466feab8da2dfca6d5a153f9ec1a1ad81
luispauloml <luispauloml@gmail.com>
2022-08-02 19:54:54 +0900

    Remove data property

--- asdl/__init__.py
+++ asdl/__init__.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pickle
 
 class MeasuredData(object):
@@ -15,19 +14,7 @@ class MeasuredData(object):
         return obj
 
     def __init__(self):
-        self._data = {'results': None}
         self.header = []
-
-    @property
-    def data(self):
-        """the data contained in this object"""
-        return self._data['results']
-
-    @data.setter
-    def data(self, values):
-        if not isinstance(values, np.ndarray):
-            raise TypeError('the values should be a numpy.nparray')
-        self._data['results'] = values
 
     def save(self, file_name):
         """Save data from current object to a binary file.
@@ -46,9 +33,6 @@ class MeasuredData(object):
         obj = MeasuredData()
         for key in obj.__dict__.keys():
             obj.__dict__[key] = self.__dict__[key]
-
-        # Guarantee that the data saved is real not complex
-        obj._data['results'] = self.data
 
         with open(file_name, 'wb') as file_:
             pickle.dump(obj, file_, pickle.HIGHEST_PROTOCOL)
